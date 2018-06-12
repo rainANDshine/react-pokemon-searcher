47efb6bde0e08f45cf3b986a9ea3d09c270e4588
laurkim <laurakeem@gmail.com>
2018-06-12 14:14:06 -0400

    Post lecture, before form implementation

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -2,14 +2,13 @@ import React from 'react';
 import PokemonCard from './PokemonCard';
 import { Card } from 'semantic-ui-react';
 
-class PokemonCollection extends React.Component {
-  render() {
-    return (
-      <Card.Group itemsPerRow={6}>
-        <h1>Hello From Pokemon Collection</h1>
-      </Card.Group>
-    )
-  }
+const PokemonCollection = props => {
+  const pokemons = props.pokemons.map(pokemon => <PokemonCard key={pokemon.id} pokemon={pokemon} />)
+  return (
+    <Card.Group itemsPerRow={6}>
+      {pokemons}
+    </Card.Group>
+  )
 }
 
 export default PokemonCollection;
--- src/components/PokemonIndex.js
+++ src/components/PokemonIndex.js
@@ -6,15 +6,41 @@ import { Search } from 'semantic-ui-react';
 const URL = 'http://localhost:3000/pokemon';
 
 class PokemonIndex extends React.Component {
+  state = {
+    pokemons: [],
+    searchTerm: ''
+  }
+
+  componentDidMount() {
+    fetch(URL)
+      .then(res => res.json())
+      .then(json => this.setState({
+        pokemons: json
+      }))
+  }
+
+  handleSearch = (e) => {
+    console.log(e.target.value)
+    this.setState({
+      searchTerm: e.target.value
+    })
+  }
+
+  filterPokemon = () => {
+    return this.state.pokemons.filter(p => p.name.includes(this.state.searchTerm.toLowerCase()))
+  }
+
+
   render() {
+    let filteredPokemon = this.filterPokemon();
     return (
       <div>
-        <br />
-        <Search onSearchChange={null} showNoResults={false} />
-        <br />
-        <PokemonCollection />
-        <br />
         {/*<PokemonForm />*/}
+        <br />
+        <Search onSearchChange={this.handleSearch} showNoResults={false} />
+        <br />
+        {this.state.pokemons.length > 0 ? <PokemonCollection pokemons={filteredPokemon} /> : <Loading /> }
+        <br />
       </div>
     )
   }
