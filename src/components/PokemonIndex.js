d4d28ac68d34c6b120271622179125955fa5e33c
laurkim <laurakeem@gmail.com>
2018-06-12 13:15:36 -0400

    Pre-Review

--- src/components/PokemonIndex.js
+++ src/components/PokemonIndex.js
@@ -3,12 +3,12 @@ import PokemonCollection from './PokemonCollection';
 // import PokemonForm from './PokemonForm';
 import Loading from './Loading';
 import { Search } from 'semantic-ui-react';
+const URL = 'http://localhost:3000/pokemon';
 
 class PokemonIndex extends React.Component {
   render() {
     return (
       <div>
-        <h1>Pokemon Searcher</h1>
         <br />
         <Search onSearchChange={null} showNoResults={false} />
         <br />
